8736a719665b37ae57dfc0025da3630efe269a31
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-04-14 02:54:33 +0300

    add: the source of taken code from stackoverflow as a comment line

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function setInputValidation(controlName, spanId, errorMessage) {
     }
 }
 function validateEmail(email) {
+    // source: https://stackoverflow.com/a/46181/17733859
     return email.match(
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
